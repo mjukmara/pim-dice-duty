71fcd4cd78dd2ee28c33e7139ce1fc09948d8102
Yam-0 <tage.akerstrom@gmail.com>
2022-07-15 22:15:43 +0200

    Merge and player object

---
+++ Assets/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+		Debug.Log("Player Start");
+    }
+
+    void Update()
+    {
+
+    }
+}
