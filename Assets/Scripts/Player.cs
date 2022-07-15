8f3f8ceef1cb6338c51bfecb8a3b3cac5ddd3607
Yam-0 <tage.akerstrom@gmail.com>
2022-07-15 22:50:30 +0200

    Player movement, physics material and background details

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float speed = 1000.0f;
+
+	Rigidbody2D rb;
+
+    void Start()
+    {
+		rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+		float x = Input.GetAxisRaw("Horizontal");
+		float y = Input.GetAxisRaw("Vertical");
+
+		Vector2 movement = new Vector2(x, y);
+		movement.Normalize();
+
+		rb.velocity = movement * speed * Time.deltaTime;
+    }
+}
