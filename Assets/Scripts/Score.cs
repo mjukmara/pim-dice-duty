c986bf8bc58136f148df018affb1d83f787e5e32
Yam-0 <tage.akerstrom@gmail.com>
2022-07-16 23:32:26 +0200

    ScoreSpawner fix

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+	public TMPro.TextMeshProUGUI text;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
