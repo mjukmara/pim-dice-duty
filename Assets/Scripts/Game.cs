68fcf324630c8c56429c56e26d110743a215a67d
Yam-0 <tage.akerstrom@gmail.com>
2022-07-16 23:14:37 +0200

    Score, game close and game win

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
+	public static int multiplier = 1;
+
 	static Game instance;
+	public TMPro.TextMeshProUGUI scoreText;
+
+	int score = 0;
+
 	Animator animator;
 
     void Start()
@@ -15,12 +22,27 @@ public class Game : MonoBehaviour
 
     void Update()
     {
-
+		scoreText.text = "Score: " + score;
     }
 
 	public void StartGame()
 	{
 		instance?.animator.ResetTrigger("Start");
 		instance?.animator.SetTrigger("Start");
+	}
+
+	public static void AddScore(int amount)
+	{
+		instance.score += amount;
+	}
+
+	public static void Win()
+	{
+		SceneManager.LoadScene("Win");
+	}
+
+	public void Close()
+	{
+		Application.Quit();
 	}
 }
---
+++ Assets/Scripts/ScoreSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSpawner : MonoBehaviour
+{
+	public ScoreSpawner instance;
+	public GameObject score;
+
+	void Start()
+	{
+		instance = this;
+	}
+
+	void Spawn(int addScore, Vector3 position)
+	{
+		GameObject scoreInstance = Instantiate(score, position, Quaternion.identity);
+		TMPro.TextMeshProUGUI text = scoreInstance.GetComponent<TMPro.TextMeshProUGUI>();
+		string suffix = "";
+
+		float newScore = addScore * Game.multiplier;
+
+		if (newScore > 100) { suffix = "!"; }
+		if (newScore > 250) { suffix = "!!"; }
+		if (newScore > 600) { suffix = "!!!"; }
+		if (newScore > 1000) { suffix = "!!!"; }
+
+		text.text = addScore + " X" + Game.multiplier + suffix;
+
+		Game.AddScore(addScore * Game.multiplier);
+	}
+
+	public static void SpawnScore(int addScore, Vector3 position)
+	{
+		instance.Spawn(addScore, position);
+	}
+}
