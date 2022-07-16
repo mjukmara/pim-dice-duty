8bf74da07527c2a6b6e9f8a0dcac10f545c36ca0
Yam-0 <tage.akerstrom@gmail.com>
2022-07-17 01:30:33 +0200

    Disabled x cropping

--- Assets/Scripts/ContainerScript.cs
+++ Assets/Scripts/ContainerScript.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class ContainerScript : MonoBehaviour
 {
+	public Canvas canvas;
 	public Transform bottomLeft;
 	public Transform topRight;
 	RectTransform rt;
+
+	public Vector2 bl;
+	public Vector2 tr;
 
 	void Start()
 	{
@@ -15,11 +20,12 @@ public class ContainerScript : MonoBehaviour
 
     void LateUpdate()
     {
-		Vector2 bl = Camera.main.WorldToScreenPoint(bottomLeft.position);
-		Vector2 tr = Camera.main.WorldToScreenPoint(topRight.position);
-		rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 4, 0);
-		rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 4, 0);
-		rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 4, 0);
-		rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 4, 0);
+		bl = Camera.main.WorldToScreenPoint(bottomLeft.position);
+		tr = Camera.main.WorldToScreenPoint(topRight.position);
+
+		/* rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, bl.x, tr.x - bl.x); */
+		/* rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, tr.y, tr.y - bl.y); */
+		/* rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 2000, 0); */
+		/* rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 2000, 0); */
     }
 }
